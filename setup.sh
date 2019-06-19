00aa9a2745d85121ca2550cf2a1c4fd272bda3f1
Praveen Perera <ppraveen25@gmail.com>
2019-06-18 22:57:14 -0400

    Creat setup script, make symlinks and setup prompt

--- setup.sh
+++ setup.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
-############################
-# .make.sh
-# This script creates symlinks from the home directory to any desired dotfiles in ~/code/dotfiles
-############################
-
-########## Variables
 
 dir=~/code/dotfiles                    # dotfiles directory
 olddir=~/code/dotfiles_old            # old dotfiles backup directory
 files="vimrc zsh_profile zshrc-e gvimrc.after zshrc zpreztorc zlogin zlogout zprofile zshenv"    # list of files/folders to symlink in homedir
 
 ##########
-
-# create dotfiles_old in homedir
-echo "Creating $olddir for backup of any existing dotfiles in ~"
-mkdir -p $olddir
-echo "...done"
-
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done"
 
+echo "Creating all symlinks"
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Moving any existing dotfiles from ~ to $olddir"
@@ -29,3 +18,6 @@ for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
 done
+
+echo "Adding zprezto custom theme"
+cp prompt_praveen_setup ~/.zprezto/modules/prompts
