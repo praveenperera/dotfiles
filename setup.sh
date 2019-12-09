582968c34fbe6bc45cdac2b5677ee9697ea754d0
Praveen Perera <ppraveen25@gmail.com>
2019-12-09 15:12:56 -0500

    Setup starship config file

--- setup.sh
+++ setup.sh
@@ -6,16 +6,23 @@ dir=~/code/dotfiles
 # list of files/folders to symlink in homedir
 files="vimrc zsh_profile zshrc-e gvimrc.after zshrc zpreztorc zlogin zlogout zprofile zshenv gitconfig spacemacs"    
 
+config_files="starship.toml"
+
 # change to the dotfiles directory
 echo "Changing to the $dir directory"
 cd $dir
 echo "...done\n"
 
 echo "Creating all symlinks \n"
-# move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
 for file in $files; do
     echo "Creating symlink to $file in home directory."
     ln -s $dir/$file ~/.$file
+done
+
+echo "Creating Config Files \n"
+for file in $config_files; do
+    echo "Creating symlink to $file in config directory."
+    ln -s $dir/config/$file ~/.config/$file
 done
 
 echo "\nAdding zprezto custom theme"
