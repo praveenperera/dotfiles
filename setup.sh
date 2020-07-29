c430d7fd3011aa3fff9d55d572a15973cdbfe62b
Praveen Perera <ppraveen25@gmail.com>
2020-07-28 20:18:28 -0400

    Add direnvrc and gitignore global to setup

--- setup.sh
+++ setup.sh
@@ -11,7 +11,7 @@ mkdir -p ~/.config
 dir=~/code/dotfiles                    
 
 # list of files/folders to symlink in homedir
-files="vimrc gvimrc.after zshrc gitconfig spacemacs zsh_plugins.sh gitignore"   
+files="vimrc gvimrc.after zshrc gitconfig spacemacs zsh_plugins.sh gitignore direnvrc gitignore_global"   
 
 config_files="starship.toml"
 
