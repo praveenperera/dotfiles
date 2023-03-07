9ccc6cf95728f49d7183c83a9656d32c65cf2a6e
Praveen Perera <praveen@avencera.com>
2023-03-07 10:17:19 -0600

    Install project manager

--- nvim/lua/main/packer.lua
+++ nvim/lua/main/packer.lua
@@ -104,4 +104,15 @@ return require('packer').startup(function(use)
 
     use 'navarasu/onedark.nvim'
 
+
+    use {
+        "ahmedkhalf/project.nvim",
+        config = function()
+            require("project_nvim").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end
+    }
 end)
