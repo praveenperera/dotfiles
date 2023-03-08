63fd6eea0ef183e13f642cec3bb79835f96ef018
Praveen Perera <praveen@avencera.com>
2023-03-07 20:40:16 -0600

    Put heirline config in its own file

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,16 +1,10 @@
---              AstroNvim Configuration Table
--- All configuration changes should go inside of the table below
--- You can think of a Lua "table" as a dictionary like data structure the
---
--- normal format is "key = value". These also handle array like data structures
--- where a value with no key simply has an implicit numeric key
+require("user.plugin_config.heirline")
+
 local config = {
-        -- Configure AstroNvim updates
         updater = {
                 remote = "origin",     -- remote to use
                 channel = "stable",    -- "stable" or "nightly"
                 version = "latest",    -- "latest", tag name, or regex search like "v1.*" to only do updates before v2 (STABLE ONLY)
-                -- branch = "main",       -- branch name (NIGHTLY ONLY)
                 commit = nil,          -- commit hash (NIGHTLY ONLY)
                 pin_plugins = nil,     -- nil, true, false (nil will pin plugins on stable only)
                 skip_prompts = false,  -- skip prompts about breaking changes
@@ -233,11 +227,9 @@ local config = {
         -- Configure plugins
         plugins = {
                 init = {
-                        -- disabled
                         {
                                 'TimUntersberger/neogit',
                                 requires = 'nvim-lua/plenary.nvim',
-                                config = function() require("neogit").setup() end
                         },
                         { 'justinmk/vim-sneak' },
                 },
@@ -260,29 +252,7 @@ local config = {
                 ["mason-lspconfig"] = {},
                 ["mason-null-ls"] = {},
                 ["mason-nvim-dap"] = {},
-                heirline = function(config)
-                        -- the first element of the default configuration table is the statusline
-                        config[1] = {
-                                -- set the fg/bg of the statusline
-                                hl = { fg = "fg", bg = "bg" },
-                                -- when adding the mode component, enable the mode text with padding to the left/right of it
-                                astronvim.status.component.mode { mode_text = { padding = { left = 1, right = 1 } } },
-                                -- add all the other components for the statusline
-                                astronvim.status.component.git_branch(),
-                                astronvim.status.component.file_info(),
-                                astronvim.status.component.git_diff(),
-                                astronvim.status.component.diagnostics(),
-                                astronvim.status.component.fill(),
-                                astronvim.status.component.cmd_info(),
-                                astronvim.status.component.fill(),
-                                astronvim.status.component.lsp(),
-                                astronvim.status.component.treesitter(),
-                                astronvim.status.component.nav(),
-                        }
-
-                        -- return the final configuration table
-                        return config
-                end,
+                heirline = HeirlineConfig
         },
         -- LuaSnip Options
         luasnip = {
