e453f1ef0ae18999d0e7ae684082e4f6692c4407
Praveen Perera <praveen@avencera.com>
2023-03-30 11:47:20 -0500

    Add module to get current os

--- cmd/src/bootstrap.rs
+++ cmd/src/bootstrap.rs
@@ -3,7 +3,6 @@ use xshell::{cmd, Shell};
 
 pub fn run(sh: &Shell) -> Result<()> {
     println!("bootstrap");
-
+    println!("{}", std::env::consts::OS);
     Ok(())
 }
-
---
+++ cmd/src/os.rs
@@ -0,0 +1,22 @@
+pub enum Os {
+    Linux,
+    MacOS,
+    Windows,
+}
+
+impl Os {
+    pub fn current() -> Self {
+        std::env::consts::OS.into()
+    }
+}
+
+impl From<&str> for Os {
+    fn from(s: &str) -> Self {
+        match s {
+            "linux" => Os::Linux,
+            "macos" => Os::MacOS,
+            "windows" => Os::Windows,
+            _ => panic!("unknown os: {}", s),
+        }
+    }
+}
