cdc855898dec202e720ea3bf8e77f6312ee54059
Praveen Perera <praveen@avencera.com>
2023-04-06 15:22:40 -0500

    Fix toggleterm size on vertical splits

--- nvim/config/toggleterm.lua
+++ nvim/config/toggleterm.lua
@@ -1,8 +1,16 @@
 local M = {}
 
+local function size(term)
+    if term.direction == "horizontal" then
+        return 15
+    elseif term.direction == "vertical" then
+        return vim.o.columns * 0.4
+    end
+end
+
 M.config = function(_, _)
     local opts = {
-        size = 15,
+        size = size,
         persist_size = true,
         start_in_insert = true,
     }
