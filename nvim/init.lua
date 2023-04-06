60d68bf6624a3e24040cba2b094594c31b5c74aa
Praveen Perera <praveen@avencera.com>
2023-04-06 13:04:22 -0500

    Remove vim-repeat

--- nvim/init.lua
+++ nvim/init.lua
@@ -238,7 +238,6 @@ local config = {
 		{
 			"ggandor/leap.nvim",
 			event = "BufRead",
-			dependencies = { "tpope/vim-repeat" },
 			config = function()
 				require('leap').add_default_mappings()
 			end
