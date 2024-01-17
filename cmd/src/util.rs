f494e740c9f06e32862527cd04bdc22f31b2dd71
Praveen Perera <praveen@avencera.com>
2024-01-17 15:34:00 -0600

    Update random generators

--- cmd/src/util.rs
+++ cmd/src/util.rs
@@ -5,7 +5,7 @@ use rand::{
 
 pub fn random_ascii(length: usize) -> String {
     const CHARSET: &[u8] =
-        b"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&*()_+{}|:?[];,./-=~";
+        b"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz!@#$%^&|-_=+*";
     let mut rng = rand::thread_rng();
     let char_num = Uniform::from(0..CHARSET.len());
 
@@ -15,7 +15,17 @@ pub fn random_ascii(length: usize) -> String {
 }
 
 pub fn random_alpha(length: usize) -> String {
-    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789abcdefghijklmnopqrstuvwxyz";
+    const CHARSET: &[u8] = b"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+    let mut rng = rand::thread_rng();
+    let char_num = Uniform::from(0..CHARSET.len());
+
+    (0..length)
+        .map(|_| CHARSET[rng.sample(char_num)] as char)
+        .collect()
+}
+
+pub fn random_pin(length: usize) -> String {
+    const CHARSET: &[u8] = b"01234567890";
     let mut rng = rand::thread_rng();
     let char_num = Uniform::from(0..CHARSET.len());
 
