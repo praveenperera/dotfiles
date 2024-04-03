fc5540e4e9a1d62e07a40ba75a285ce5a03d36db
Praveen Perera <praveen@avencera.com>
2024-04-03 14:41:29 -0500

    Update rustacean key maps

--- nvim/lua/polish.lua
+++ nvim/lua/polish.lua
@@ -1,11 +1,8 @@
--- Runs after everythign else
-return function()
-    -- Set filetype for terraform files
-    vim.cmd("au BufRead,BufNewFile *.tfvars set filetype=terraform")
+-- Set filetype for terraform files
+vim.cmd("au BufRead,BufNewFile *.tfvars set filetype=terraform")
 
-    -- Set filetype for jinja
-    vim.cmd("au BufNewFile,BufRead *.j2,*.jinja set ft=jinja")
+-- Set filetype for jinja
+vim.cmd("au BufNewFile,BufRead *.j2,*.jinja set ft=jinja")
 
-    --  Config rustaceanvim
-    vim.g.rustaceanvim = require("user.config.rustaceanvim").config()
-end
+--  Config rustaceanvim
+vim.g.rustaceanvim = require("config.rustaceanvim").config()
