fb3a73327a3d0849b7bea7c4750c27f7f2a5a386
Praveen Perera <me@praveenperera.com>
2024-07-03 14:03:51 -0500

    Disable annoying supermaven message on startup

--- nvim/lua/plugins/supermaven.lua
+++ nvim/lua/plugins/supermaven.lua
@@ -2,6 +2,7 @@ return {
     "supermaven-inc/supermaven-nvim",
     config = function()
         require("supermaven-nvim").setup({
+            log_level = "off",
             keymaps = {
                 accept_word = "<C-y>",
                 accept_suggestion = "<C-l>",
