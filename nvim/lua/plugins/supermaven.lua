888d0f9e479312735f8571171476b29f3bb417df
Praveen Perera <me@praveenperera.com>
2024-07-18 13:51:22 -0500

    Switch copilot for supermaven again

---
+++ nvim/lua/plugins/supermaven.lua
@@ -0,0 +1,13 @@
+return {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+        require("supermaven-nvim").setup({
+            log_level = "off",
+            keymaps = {
+                accept_word = "<C-y>",
+                accept_suggestion = "<C-l>",
+                clear_suggestion = "<C-]>",
+            },
+        })
+    end,
+}
