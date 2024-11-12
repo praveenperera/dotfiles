3cd2a6d3c1cc05f3c8f6245e3d918e350859a33a
Praveen Perera <me@praveenperera.com>
2024-11-12 14:35:07 -0600

    Workaround for broken rust-analyzer spam

--- nvim/after/ftplugin/rust.lua
+++ nvim/after/ftplugin/rust.lua
@@ -3,3 +3,13 @@ local bufnr = vim.api.nvim_get_current_buf()
 vim.keymap.set("n", "<leader>lz", function()
     vim.cmd.RustLsp("openCargo")
 end, { silent = true, buffer = bufnr, desc = "Open Cargo.toml" })
+
+for _, method in ipairs({ "textDocument/diagnostic", "workspace/diagnostic" }) do
+    local default_diagnostic_handler = vim.lsp.handlers[method]
+    vim.lsp.handlers[method] = function(err, result, context, config)
+        if err ~= nil and err.code == -32802 then
+            return
+        end
+        return default_diagnostic_handler(err, result, context, config)
+    end
+end
