298dcce3c5ef2d391c3bd80ff7ccb37fe456befa
Praveen Perera <me@praveenperera.com>
2025-03-17 08:43:37 -0500

    Use copilot with latest available model

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,23 +1,26 @@
--- local opts = {
---     suggestion = {
---         enabled = true,
---         auto_trigger = true,
---         keymap = {
---             accept = "<C-y>",
---             accept_word = false,
---             accept_line = false,
---             next = "<C-s>",
---             prev = "<C-a>",
---             dismiss = "<C-]>",
---         },
---     },
--- }
+local opts = {
+    suggestion = {
+        copilot_model = "gpt-4o-copilot",
+        enabled = true,
+        auto_trigger = true,
+        keymap = {
+            accept = "<C-l>",
+            accept_word = "<C-y>",
+            accept_line = false,
+            next = "<C-s>",
+            prev = "<C-a>",
+            dismiss = "<C-]>",
+        },
+    },
+}
+
+return {
+    -- "zbirenbaum/copilot.lua",
+    "AntoineGS/copilot.lua",
+    branch = "gpt4o",
+    cmd = "Copilot",
+    event = "InsertEnter",
+    opts = opts,
+}
+
 --
--- return {
---     "zbirenbaum/copilot.lua",
---     cmd = "Copilot",
---     event = "InsertEnter",
---     opts = opts,
--- }
---
-return {}
