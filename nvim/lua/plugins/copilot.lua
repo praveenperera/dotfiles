32362269e1efcfc027d14c429f93822d8cd3ccc6
Praveen Perera <me@praveenperera.com>
2025-03-19 15:40:26 -0500

    Back to supermaven

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,26 +1,26 @@
-local opts = {
-    suggestion = {
-        copilot_model = "gpt-4o-copilot",
-        enabled = true,
-        auto_trigger = true,
-        keymap = {
-            accept = "<C-l>",
-            accept_word = "<C-y>",
-            accept_line = false,
-            next = "<C-s>",
-            prev = "<C-a>",
-            dismiss = "<C-]>",
-        },
-    },
-}
-
-return {
-    -- "zbirenbaum/copilot.lua",
-    "AntoineGS/copilot.lua",
-    branch = "gpt4o",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    opts = opts,
-}
-
+-- local opts = {
+--     suggestion = {
+--         copilot_model = "gpt-4o-copilot",
+--         enabled = true,
+--         auto_trigger = true,
+--         keymap = {
+--             accept = "<C-l>",
+--             accept_word = "<C-y>",
+--             accept_line = false,
+--             next = "<C-s>",
+--             prev = "<C-a>",
+--             dismiss = "<C-]>",
+--         },
+--     },
+-- }
 --
+-- return {
+--     -- "zbirenbaum/copilot.lua",
+--     "AntoineGS/copilot.lua",
+--     branch = "gpt4o",
+--     cmd = "Copilot",
+--     event = "InsertEnter",
+--     opts = opts,
+-- }
+--
+return {}
--- nvim/lua/plugins/supermaven.lua
+++ nvim/lua/plugins/supermaven.lua
@@ -1,15 +1,15 @@
--- return {
---     "supermaven-inc/supermaven-nvim",
---     config = function()
---         require("supermaven-nvim").setup({
---             -- log_level = "off",
---             keymaps = {
---                 accept_word = "<C-y>",
---                 accept_suggestion = "<C-l>",
---                 clear_suggestion = "<C-]>",
---                 next_suggestion = "<C-j>",
---             },
---         })
---     end,
--- }
-return {}
+return {
+    "supermaven-inc/supermaven-nvim",
+    config = function()
+        require("supermaven-nvim").setup({
+            -- log_level = "off",
+            keymaps = {
+                accept_word = "<C-y>",
+                accept_suggestion = "<C-l>",
+                clear_suggestion = "<C-]>",
+                next_suggestion = "<C-j>",
+            },
+        })
+    end,
+}
+-- return {}
