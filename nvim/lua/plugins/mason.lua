9f17381c90546ea6146ad281c0c02f15c8c0a83c
Praveen Perera <me@praveenperera.com>
2025-07-21 10:48:02 -0500

    Update mason.lua

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -9,6 +9,7 @@ return {
                 })
         end,
     },
+
     -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
     {
         "jay-babu/mason-null-ls.nvim",
@@ -17,16 +18,6 @@ return {
                 require("astrocore").list_insert_unique(opts.ensure_installed, {
                     "prettier",
                     "stylua",
-                })
-        end,
-    },
-    {
-        "jay-babu/mason-nvim-dap.nvim",
-        -- overrides `require("mason-nvim-dap").setup(...)`
-        opts = function(_, opts)
-            opts.ensure_installed =
-                require("astrocore").list_insert_unique(opts.ensure_installed, {
-                    "python",
                 })
         end,
     },
