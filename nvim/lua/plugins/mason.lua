43f56fee851b1553f837de424391383f4e6809af
Praveen Perera <me@praveenperera.com>
2026-03-17 10:04:42 -0500

    Add dockerls to neovim mason ensure_installed

--- nvim/lua/plugins/mason.lua
+++ nvim/lua/plugins/mason.lua
@@ -5,6 +5,7 @@ return {
         opts = function(_, opts)
             opts.ensure_installed =
                 require("astrocore").list_insert_unique(opts.ensure_installed, {
+                    "dockerls",
                     "lua_ls",
                 })
         end,
